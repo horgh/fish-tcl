92712ea9aabfa742115f4abe732721b6a6e80085
horgh <will@summercat.com>
2010-12-18 21:19:59 -0800

    enable encryption output to channels with keys set

--- fish.tcl
+++ fish.tcl
@@ -3,14 +3,15 @@
 #
 # irssi-tcl script for FiSH IRC channels
 #
-# Commands:
-#  /fish <#channel> <message>
-#
 # Setup:
-#  /set fish_keys key1,#channel1 key2,#channel2
+#  /set fish_keys key1,#channel1 key2,#channel2 ..
+#
+# If you don't want to send encrypted to channels set with key, set:
+#  /set fish_send_encrypted off
 #
 # Any channel that has a key set which receives a message
 # prefixed with +OK will be decrypted with that key
+# and any messages entered into that channel will be sent encrypted
 #
 
 set path_to_libfish /home/will/code/fish-tcl
@@ -18,19 +19,12 @@ load ${path_to_libfish}/libfish[info sharedlibextension]
 
 namespace eval fish {
 	settings_add_str "fish_keys" ""
+	settings_add_str "fish_send_encrypted" ""
 
 	signal_add msg_pub "+OK" fish::handler
-}
+	signal_add send_text "" fish::send_text
 
-proc fish::enabled {chan} {
-	set raw [settings_get_str "fish_keys"]
-	set entries [split $raw]
-	foreach pair $entries {
-		if {[lindex [split $pair ,] 1] == $chan} {
-			return 1
-		}
-	}
-	return 0
+	set tag "\[\002fish\002\]"
 }
 
 proc fish::get_key {chan} {
@@ -45,15 +39,33 @@ proc fish::get_key {chan} {
 }
 
 proc fish::handler {server nick uhost chan argv} {
-	if {![fish::enabled $chan]} { return }
 	set key [fish::get_key $chan]
+	if {$key == ""} { return }
+
 	if {[catch {decrypt $key $argv} decrypted]} {
-		set output "\[\002fish\002\] Error decrypting: $decrypted"
+		set output "$fish::tag Error decrypting: $decrypted"
 	} else {
-		set output "\[\002fish\002\] $decrypted"
+		set output "$fish::tag $decrypted"
 	}
 	signal_stop
 	emit_message_public $server $chan $nick $uhost $output
+}
+
+proc fish::send_text {server target line} {
+	# check whether we have outgoing encryption enabled
+	if {[settings_get_str "fish_send_encrypted"] == "off"} { return }
+
+	set key [fish::get_key $target]
+	if {$key == ""} { return }
+
+	signal_stop
+
+	if {[catch {encrypt $key $line} text]} {
+		irssi_print "$fish::tag Error encrypting '$line': $text"
+	} else {
+		putchan $server $target "+OK $text"
+		irssi_print "$fish::tag (you) $line"
+	}
 }
 
 proc fish::crypt {key text} {
